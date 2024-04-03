6e28ba98fc2b61394ed765f8f73767e3cd8f84b6
messi9999 <micklepanda812@gmail.com>
2024-04-03 07:53:18 -0700

    udpated pm2

---
+++ server/ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    apps : [{
+      name: "my_fastapi_app",
+      cmd: "uvicorn",
+      args: "main:app --host 0.0.0.0 --port 8000",
+      interpreter: "/myenv/bin/python3",
+      watch: true,
+      env: {
+        "ENV": "development",
+      },
+      env_production: {
+        "ENV": "production",
+      }
+    }]
+  }
