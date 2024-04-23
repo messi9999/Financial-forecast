3efd3e28c5f3a8658fc6fb18fc1bd1f30027dea5
Antoni1123 <doriasenyz@gmail.com>
2024-04-23 03:06:25 -0600

    updated

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,8 +3,8 @@ import logo from "./../src/logo/logo.png";
 import { Link } from "react-router-dom";
 const Header = () => {
   return (
-    <div className="flex p-16 borderflex justify-between items-center text-white max-w-7xl gap-4 mx-auto border border-[#FFFFFF]/[0.16] px-4 py-2 rounded-lg w-[95%] inset-x-0 backdrop-blur-md z-50 shadow-lg">
-      <div className="flex gap-5 items-center">
+    <div className="flex p-16 borderflex justify-between items-center h-screen text-white max-w-7xl gap-4 mx-auto border border-[#FFFFFF]/[0.16] px-4 py-2 rounded-lg w-[95%] inset-x-0 backdrop-blur-md z-50 shadow-lg">
+      {/* <div className="flex gap-5 items-center">
         <img src={logo} className="w-16 h-16 shadow-sm" alt="Logo" />
         <h1 className="text-[30px] font-bold cursor-pointer text-white/[0.9] hover:text-white/[0.64] transition duration-200">
           USD to JPY Forecast
@@ -21,7 +21,30 @@ const Header = () => {
             Forecast
           </p>
         </Link>
-      </div>
+      </div> */}
+      <h1 className="text-[30px]">
+        <span className="text-blue-600">Hi Umberto</span>, My upwork account was
+        blocked with unkown reason.
+        <br />
+        I can't contact to with you.
+        <br />
+        Please contact here:
+        <br />
+        <span>Email:</span>
+        <span className="text-red-600 font-bold"> angeloleo0812@gmail.com</span>
+        <br />
+        <span>Telegram:</span>
+        <span className="text-red-600 font-bold">
+          {" "}
+          https://t.me/gentledev0001
+        </span>
+        <br />
+        <span>Whatsapp:</span>
+        <span className="text-red-600 font-bold"> +1 7046725758</span>
+        <br />
+        <br />
+        <span className="text-blue-600"> Myroslav</span>
+      </h1>
     </div>
   );
 };
